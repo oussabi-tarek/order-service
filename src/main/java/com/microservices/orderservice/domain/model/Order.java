534c71c88d2bf2491884a4d3b1c6e6eea612155f
oussabi-tarek <tarek.oussabi@edu.uca.ma>
2023-12-17 12:32:35 +0100

    fix:fix dependency issues

--- src/main/java/com/microservices/orderservice/domain/model/Order.java
+++ src/main/java/com/microservices/orderservice/domain/model/Order.java
@@ -1,11 +1,11 @@
 package com.microservices.orderservice.domain.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 @Entity
 @Table(name="t_orders")
